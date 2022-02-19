3acf1be2d76627a277fe95b2f59312a53bf1aa63
argoneon <12193696@student.uts.edu.au>
2022-02-19 17:42:22 +0900

    기존 프로젝트에서 themes.xml, colors.xml, attars.xml 가져옴 초기 프로젝트 생성에서 만든 MainActivity 제거하고 Tabbed Activity로 재생성 첫번째 프래그먼트에 리사이클러 뷰와 마테리얼 버튼만 추가 메인 액티비티에 오디오 단순 재생 데모 추가 (성공 후 주석처리)

--- app/src/main/java/ark/noah/audiosegmentv2/MainActivity.java
+++ app/src/main/java/ark/noah/audiosegmentv2/MainActivity.java
@@ -1,14 +1,70 @@
 package ark.noah.audiosegmentv2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import java.util.Objects;
+
+import ark.noah.audiosegmentv2.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    //Uri musicUri;
+    //MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+
+        NavHostFragment navHostFragment = (NavHostFragment) (getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main));
+        NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        //Simple Audio Play Demo
+        /*
+        ActivityResultLauncher<Intent> fileActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        if (data != null) {
+                            musicUri = data.getData();
+                        }
+                    }
+                });
+
+        binding.btnDemopickup.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("audio/*");
+            fileActivityResultLauncher.launch(intent);
+        });
+
+        binding.btnDemoplay.setOnClickListener(v->{
+            mediaPlayer = MediaPlayer.create(this, musicUri);
+            mediaPlayer.start();
+        });
+        */
     }
 }
